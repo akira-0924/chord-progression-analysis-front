9ffb1623eab871194409f2f3d412b1cf91619e6d
水島アキラ <YAMABIKO.yamazusi@gmail.com>
2024-09-01 15:24:15 +0900

    カスタムのuseSWRを作成

---
+++ src/app/hooks/useCustomSWR.ts
@@ -0,0 +1,32 @@
+'use client';
+import { customApiClient } from '../(api)/apiClient';
+import useSWR, { BareFetcher, Fetcher } from 'swr';
+
+type AxiosOption = {
+  baseURL?: string;
+};
+
+export const useCustomSWR = <Data>(
+  key: string,
+  bareFetcher:
+    | BareFetcher<Data>
+    | undefined
+    | null = undefined,
+  config: any,
+  axiosOption: AxiosOption = {}
+) => {
+  const fetcher = getFetcher(bareFetcher, axiosOption);
+  return useSWR<Data, Error>(key, fetcher, config);
+};
+
+const getFetcher = <Data>(
+  fetcher: BareFetcher<Data> | null | undefined,
+  axiosOptions: AxiosOption = {}
+) => {
+  if (fetcher === undefined) {
+    const apiClient = customApiClient(axiosOptions);
+    return (url: string) =>
+      apiClient.get(url).then((res) => res.data);
+  }
+  return fetcher;
+};
