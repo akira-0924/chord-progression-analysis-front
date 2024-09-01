414bef718e3d50d31b4fcbe9102a20e574ef4029
水島アキラ <YAMABIKO.yamazusi@gmail.com>
2024-09-01 15:38:21 +0900

    　customSWRを使ったサンプルのAPIリクエストの作成

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,19 +1,29 @@
 'use client';
 import React from 'react';
-import axios from 'axios';
-import useSWR from 'swr';
+import { useCustomSWR } from '../hooks/useCustomSWR';
 
 // https://digi-api.com/api/v1/digimon/101
+const testEndpoint = 'https://dummyjson.com/todos';
+
+type Todo = {
+  id: number;
+  todo: string;
+  completed: boolean;
+  userId: number;
+};
+
+type Data = {
+  todos: Todo[];
+};
 
 const Home = () => {
-  const fetcher = (url: string) =>
-    axios.get(url).then((res) => res.data);
-  const { data, isLoading, error } = useSWR(
-    'https://digi-api.com/api/v1/digimon/101',
-    fetcher
+  const { data, error, isLoading } = useCustomSWR<Data>(
+    '/todos',
+    {
+      baseURL: 'https://dummyjson.com',
+    }
   );
   console.log(data);
-
   return <div>dummy</div>;
 };
 
--- src/app/hooks/useCustomSWR.ts
+++ src/app/hooks/useCustomSWR.ts
@@ -6,27 +6,34 @@ type AxiosOption = {
   baseURL?: string;
 };
 
+// export const useCustomSWR = <Data>(
+//   key: string,
+//   bareFetcher:
+//     | BareFetcher<Data>
+//     | undefined
+//     | null = undefined,
+//   config: any,
+//   axiosOption: AxiosOption = {}
+// ) => {
+//   const fetcher = getFetcher(bareFetcher, axiosOption);
+//   return useSWR<Data, Error>(key, fetcher, config);
+// };
 export const useCustomSWR = <Data>(
   key: string,
-  bareFetcher:
-    | BareFetcher<Data>
-    | undefined
-    | null = undefined,
-  config: any,
   axiosOption: AxiosOption = {}
 ) => {
-  const fetcher = getFetcher(bareFetcher, axiosOption);
-  return useSWR<Data, Error>(key, fetcher, config);
+  const fetcher = getFetcher(axiosOption);
+  return useSWR<Data, Error>(key, fetcher);
 };
 
 const getFetcher = <Data>(
-  fetcher: BareFetcher<Data> | null | undefined,
+  // fetcher: BareFetcher<Data> | null | undefined,
   axiosOptions: AxiosOption = {}
 ) => {
-  if (fetcher === undefined) {
-    const apiClient = customApiClient(axiosOptions);
-    return (url: string) =>
-      apiClient.get(url).then((res) => res.data);
-  }
-  return fetcher;
+  // if (fetcher === undefined) {
+  const apiClient = customApiClient(axiosOptions);
+  return (url: string) =>
+    apiClient.get(url).then((res) => res.data);
+  // }
+  // return fetcher;
 };
